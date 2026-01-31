05acc05e89ecd280f8391962eb62f75800e6621a
thenewhires <the.new.hires.public@gmail.com>
2026-01-31 14:16:58 +0100

    Add frontend/src/components/Header.jsx

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Weather App</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
